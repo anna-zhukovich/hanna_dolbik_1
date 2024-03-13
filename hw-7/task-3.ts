379ce09fb4657cd4a9b2d2596c582dfba9810c4b
Anna <anna@Alexanders-MacBook-Pro.local>
2024-03-13 22:41:07 +0100

    fixed pr comments for  hw-7

--- hw-7/task-3.ts
+++ hw-7/task-3.ts
@@ -1,3 +1,7 @@
 const numbers = [5, 43, 63, 23, 90];
 numbers.length = 0
 console.log(numbers.length);
+
+const   secondNumbers = [5, 43, 63, 23, 90];
+secondNumbers.splice(0,secondNumbers.length);
+console.log(secondNumbers.length);
