dcbb25d8080e618caac29dae241e12e6021ab648
Anna <anna@Alexanders-MacBook-Pro.local>
2024-04-03 14:34:18 +0200

    revert hw-11

--- hw-11/test2.ts
+++
@@ -1,8 +0,0 @@
-const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Промис выполнен");
-    }, 2000);
-  });
-
-
-  promise2.then((data) => {console.log(data)});
